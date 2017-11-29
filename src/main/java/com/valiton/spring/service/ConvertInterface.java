c3e97ab37aabb8501f908a14960a236ae3baf817
Uwe Jäger <uwej711@googlemail.com>
2017-11-29 17:05:24 +0100

    remove code files - use the branches

--- src/main/java/com/valiton/spring/service/ConvertInterface.java
+++
@@ -1,9 +0,0 @@
-package com.valiton.spring.service;
-
-public interface ConvertInterface {
-
-    public float celsiusToFahrenheit(float degreesCelcius);
-
-    public float fahrenheitToCelsius(float degreesFahrenheit);
-
-}
